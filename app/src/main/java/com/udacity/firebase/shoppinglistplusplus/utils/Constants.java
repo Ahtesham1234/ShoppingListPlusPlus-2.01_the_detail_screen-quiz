7c4d7b68dc77ff9004ad57514aad832c905a01c5
Ahtesham1234 <5303372a>
2016-02-17 15:55:05 +0500

    Initial commit

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/utils/Constants.java
@@ -0,0 +1,34 @@
+package com.udacity.firebase.shoppinglistplusplus.utils;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+    /**
+     * Constants related to locations in Firebase, such as the name of the node
+     * where active lists are stored (ie "activeLists")
+     */
+
+
+    /**
+     * Constants for Firebase object properties
+     */
+    public static final String FIREBASE_PROPERTY_LIST_NAME = "listName";
+    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+    // TODO Consider adding and using a constant here for the key where your lists are stored
+
+    
+    /**
+     * Constants for Firebase URL
+     */
+    public static final String FIREBASE_URL = "https://time-table.firebaseio.com";
+    // TODO Consider adding a constant for the URL for your lists
+
+
+    /**
+     * Constants for bundles, extras and shared preferences keys
+     */
+    public static final String KEY_LAYOUT_RESOURCE = "LAYOUT_RESOURCE";
+
+}
